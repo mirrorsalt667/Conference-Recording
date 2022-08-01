43e425e479a51f253b1d244303d9e82e4a2d3974
mirrorsalt667 <79618127+mirrorsalt667@users.noreply.github.com>
2022-08-01 21:34:26 +0800

    Initial Commit

---
+++ Conference Recording/Conference_RecordingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Conference_RecordingApp.swift
+//  Conference Recording
+//
+//  Created by 黃肇祺 on 2022/8/1.
+//
+
+import SwiftUI
+
+@main
+struct Conference_RecordingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Conference Recording/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Conference Recording
+//
+//  Created by 黃肇祺 on 2022/8/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
